dafc7b5f6b4b1abd11b0b5e13880a9166aba635a
youngyounkim <rladuddbs789@gmail.com>
2019-10-11 18:33:17 +0900

    Updates

---
+++ precache-manifest.98b628ea0c89eed845fe7ebf52557b1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54496122663dae5d1647b3b5ae2f6fc3",
+    "url": "/trello/index.html"
+  },
+  {
+    "revision": "e22a3c241e192301540c",
+    "url": "/trello/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "91c7ded54013cafd2795",
+    "url": "/trello/static/js/2.258b3526.chunk.js"
+  },
+  {
+    "revision": "e22a3c241e192301540c",
+    "url": "/trello/static/js/main.9bf8daae.chunk.js"
+  },
+  {
+    "revision": "bf7ba75f93c42f7f282b",
+    "url": "/trello/static/js/runtime-main.4769bc85.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/trello/static/media/logo.25bf045c.svg"
+  }
+]);
